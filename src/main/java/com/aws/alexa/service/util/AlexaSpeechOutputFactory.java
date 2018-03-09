73ef04814f5de15d1d574aefdde8a7722e0abaf9
lingarajkumar <lingapsg@gmail.com>
2018-03-09 19:57:20 +0100

    remove hardcoded response

--- src/main/java/com/aws/alexa/service/util/AlexaSpeechOutputFactory.java
+++ src/main/java/com/aws/alexa/service/util/AlexaSpeechOutputFactory.java
@@ -6,6 +6,6 @@ import com.aws.alexa.service.domain.response.SpeechType;
 public class AlexaSpeechOutputFactory {
 
     public static AlexaOutputSpeech getAlexaOutput(SpeechType speechType, String speechText) {
-        return new AlexaOutputSpeech(SpeechType.PLAINTEXT,"Please provide your mobile number.", null);
+        return new AlexaOutputSpeech(speechType, speechText, null);
     }
 }
