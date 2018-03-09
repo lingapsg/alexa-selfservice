e7bb8f74cfe50773a5155f19e8c4b9fbbbd40927
lingarajkumar <lingapsg@gmail.com>
2018-03-09 19:08:18 +0100

    modify base package scan and disable management security

--- src/main/java/com/aws/alexa/service/AlexaSelfserviceApplication.java
+++ src/main/java/com/aws/alexa/service/AlexaSelfserviceApplication.java
@@ -3,7 +3,7 @@ package com.aws.alexa.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.aws.alexa.service.")
+@SpringBootApplication(scanBasePackages = "com.aws.alexa.service")
 public class AlexaSelfserviceApplication {
 
 	public static void main(String[] args) {
