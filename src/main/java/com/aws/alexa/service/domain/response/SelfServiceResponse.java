cd79d7ba2233fdd93fdc0a9b8be25613928d879d
lingarajkumar <lingapsg@gmail.com>
2018-03-09 22:27:25 +0100

    add loggers for debugging

--- src/main/java/com/aws/alexa/service/domain/response/SelfServiceResponse.java
+++ src/main/java/com/aws/alexa/service/domain/response/SelfServiceResponse.java
@@ -1,6 +1,9 @@
 package com.aws.alexa.service.domain.response;
 
 import com.aws.alexa.service.domain.response.AlexaResponse;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.Map;
 
@@ -14,5 +17,20 @@ public class SelfServiceResponse {
         this.version = version;
         this.sessionAttributes = sessionAttributes;
         this.response = response;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 }
